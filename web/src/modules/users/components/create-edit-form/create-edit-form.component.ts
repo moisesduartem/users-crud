457a43fb9a158b13e13840051ae32fe1f966da83
Moisés Maia <moisesduartem@gmail.com>
2022-11-27 05:53:27 -0300

    feat(web): configure create/edit form validation

--- web/src/modules/users/components/create-edit-form/create-edit-form.component.ts
+++ web/src/modules/users/components/create-edit-form/create-edit-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SCHOLARITY_NAMES } from '../../enums/scholarity.enum';
 
 @Component({
@@ -7,13 +8,26 @@ import { SCHOLARITY_NAMES } from '../../enums/scholarity.enum';
   styleUrls: ['./create-edit-form.component.scss']
 })
 export class CreateEditFormComponent implements OnInit {
+  formGroup: FormGroup;
 
-  constructor() { }
+  constructor() {
+    this.formGroup = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      birthDate: new FormControl('', [Validators.required]),
+      scholarity: new FormControl('', [Validators.required]),
+    });
+  }
 
   ngOnInit(): void {
   }
 
   get scholarities(): string[] {
     return Object.values(SCHOLARITY_NAMES);
+  }
+
+  get shouldDisableSubmitButton(): boolean {
+    return this.formGroup.status !== 'VALID';
   }
 }
